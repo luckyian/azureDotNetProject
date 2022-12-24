9c7289728eba359f9b392866aec513e8b4334165
luckyian <ian.wren81@gmail.com>
2022-12-23 17:38:18 -0800

    Added lines in program.cs to learn functionality of variables in C#

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+string greeting = "Hello";
+greeting = greeting + " " + "World";
+Console.WriteLine("greeting" + " " + greeting);
+Console.WriteLine($"greeting {greeting}"); 
+Console.WriteLine("greeting: {0}", greeting);
 Console.WriteLine("Hello, World!");
